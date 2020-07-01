401f327fd6ebb73b876d86a7e9200611e55a6e33
Christopher Friedt <chrisfriedt@gmail.com>
2020-07-01 13:22:18 -0400

    tests: gpio_basic_api: support for simulated GPIO
    This adds support for simulated GPIO (CONFIG_SIM_GPIO=y) and
    additionally allows BOARD=native_posix and
    BOARD=native_posix_64 to run the gpio_basic_api test suite.
    
    Fixes #26477
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

--- tests/drivers/gpio/gpio_basic_api/src/main.c
+++ tests/drivers/gpio/gpio_basic_api/src/main.c
@@ -20,6 +20,68 @@
 #include "soc.h"
 #endif
 
+#if defined(CONFIG_GPIO_SIM)
+
+#include <drivers/gpio/gpio_sim.h>
+
+/*
+ * When GPIO are simulated, this callback can be used to implement the
+ * "wiring". E.g. in this test application, PIN_OUT is connected to
+ * PIN_IN. When PIN_OUT is set high or low, PIN_IN must be set
+ * correspondingly, as if a wire were connecting the two.
+ */
+
+static void gpio_sim_callback_handler(struct device *port,
+				      struct gpio_callback *cb,
+				      gpio_port_pins_t pins);
+
+static struct gpio_callback gpio_sim_callback = {
+	.handler = gpio_sim_callback_handler,
+	.pin_mask = BIT(PIN_IN) | BIT(PIN_OUT),
+};
+
+static void gpio_sim_callback_handler(struct device *port,
+				      struct gpio_callback *cb,
+				      gpio_port_pins_t pins)
+{
+	int r;
+	int val;
+	uint32_t output_flags;
+	uint32_t input_flags;
+
+	__ASSERT(pins & gpio_sim_callback.pin_mask, "invalid mask: %x", pins);
+
+	r = gpio_sim_flags_get(port, PIN_OUT, &output_flags);
+	__ASSERT(r == 0, "gpio_sim_flags_get() failed: %d", r);
+	r = gpio_sim_flags_get(port, PIN_IN, &input_flags);
+	__ASSERT(r == 0, "gpio_sim_flags_get() failed: %d", r);
+
+	if ((output_flags & GPIO_OUTPUT) && (input_flags & GPIO_INPUT)) {
+		r = gpio_sim_output_get(port, PIN_OUT);
+		__ASSERT(r == 0 || r == 1, "gpio_sim_output_get() failed: %d", r);
+		val = r;
+		r = gpio_sim_input_set(port, PIN_IN, val);
+		__ASSERT(r == 0, "gpio_sim_input_set() failed: %d", r);
+
+		return;
+	}
+
+	if ((output_flags == GPIO_DISCONNECTED) && (input_flags & GPIO_INPUT)) {
+		if (input_flags & GPIO_PULL_UP) {
+			val = 1;
+		} else {
+			/* either GPIO_PULL_DOWN or no input */
+			val = 0;
+		}
+
+		r = gpio_sim_input_set(port, PIN_IN, val);
+		__ASSERT(r == 0, "gpio_sim_input_set() failed: %d", r);
+
+		return;
+	}
+}
+#endif /* defined(CONFIG_GPIO_SIM) */
+
 static void board_setup(void)
 {
 #if DT_NODE_HAS_STATUS(DT_INST(0, test_gpio_basic_api), okay)
@@ -115,6 +177,12 @@ static void board_setup(void)
 
 	pinmux_pin_set(pmx, PIN_OUT, PORT_PCR_MUX(kPORT_MuxAsGpio));
 	pinmux_pin_set(pmx, PIN_IN, PORT_PCR_MUX(kPORT_MuxAsGpio));
+#elif defined(CONFIG_GPIO_SIM)
+	struct device *dev = device_get_binding(DEV_NAME);
+	zassert_not_equal(dev, NULL,
+			  "Device not found");
+	int rc = gpio_add_callback(dev, &gpio_sim_callback);
+	__ASSERT(rc == 0, "gpio_add_callback() failed: %d", rc);
 #endif
 }
 

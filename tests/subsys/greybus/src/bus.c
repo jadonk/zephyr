7d7df6dd23351f79594b24d283aea6a1c91fbdf4
Christopher Friedt <chrisfriedt@gmail.com>
2020-07-16 15:07:40 -0400

    able to dump mnfb now

--- tests/subsys/greybus/src/bus.c
+++ tests/subsys/greybus/src/bus.c
@@ -9,8 +9,12 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(greybus_test_bus);
 
+#include "bus.h"
+
 struct greybus_config {
     const uint8_t id;
+    const uint8_t version_major;
+    const uint8_t version_minor;
 };
 
 struct greybus_data {
@@ -30,23 +34,79 @@ static int greybus_init(struct device *dev) {
 		return -ENOMEM;
 	}
 
-	LOG_INF("probed bus %u", config->id);
+	LOG_INF("probed greybus: %u major: %u minor: %u",
+		config->id, config->version_major, config->version_minor);
 
     return 0;
 }
 
-#define DEFINE_GREYBUS(_num)                                     	\
-																	\
-        static const struct greybus_config 							\
-			greybus_config_##_num = {      							\
-				.id = _num,                                 		\
-        };															\
-        															\
-		static struct greybus_data greybus_data_##_num;				\
-																	\
-        DEVICE_INIT(greybus_##_num, "GREYBUS_" #_num,				\
-                            greybus_init, &greybus_data_##_num,		\
-                            &greybus_config_##_num, POST_KERNEL,	\
-                            CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+static int greybus_add_interface(struct device *dev, uint16_t vendor_string_id,
+		uint16_t product_string_id) {
+
+	struct greybus_data *const data =
+			(struct greybus_data *)dev->driver_data;
+
+	return manifest_add_interface_desc(data->manifest, vendor_string_id, product_string_id);
+}
+
+static int greybus_add_string(struct device *dev, uint8_t id, const char *string_) {
+
+	struct greybus_data *const data =
+			(struct greybus_data *)dev->driver_data;
+
+	return manifest_add_string_desc(data->manifest, id, string_);
+}
+
+static int greybus_add_bundle(struct device *dev, uint8_t id, BundleClass class_) {
+
+	struct greybus_data *const data =
+			(struct greybus_data *)dev->driver_data;
+
+	return manifest_add_bundle_desc(data->manifest, id, class_);
+}
+
+static int greybus_add_cport(struct device *dev, uint8_t id, BundleClass class_, CPortProtocol protocol) {
+
+	struct greybus_data *const data =
+			(struct greybus_data *)dev->driver_data;
+
+	return manifest_add_cport_desc(data->manifest, id, class_, protocol);
+}
+
+static manifest_t greybus_get_manifest(struct device *dev) {
+
+	struct greybus_data *const data =
+			(struct greybus_data *)dev->driver_data;
+
+	return data->manifest;
+}
+
+static const struct bus_api greybus_api = {
+	.add_interface = greybus_add_interface,
+	.add_string = greybus_add_string,
+	.add_bundle = greybus_add_bundle,
+	.add_cport = greybus_add_cport,
+	.get_manifest = greybus_get_manifest,
+};
+
+#define DEFINE_GREYBUS(_num)                            \
+														\
+        static const struct greybus_config 				\
+			greybus_config_##_num = {      				\
+				.id = _num,                             \
+				.version_major = 						\
+					DT_INST_PROP(_num, version_major),	\
+				.version_minor = 						\
+					DT_INST_PROP(_num, version_minor),	\
+        };												\
+        												\
+		static struct greybus_data greybus_data_##_num;	\
+														\
+        DEVICE_AND_API_INIT(greybus_##_num,				\
+			"GREYBUS_" #_num,							\
+			greybus_init, &greybus_data_##_num,			\
+			&greybus_config_##_num, POST_KERNEL,		\
+			CONFIG_KERNEL_INIT_PRIORITY_DEVICE,			\
+			&greybus_api);
 
 DT_INST_FOREACH_STATUS_OKAY(DEFINE_GREYBUS);
--- tests/subsys/greybus/src/control.c
+++ tests/subsys/greybus/src/control.c
@@ -9,19 +9,48 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(greybus_test_control);
 
+#include "bus.h"
+
 struct greybus_control_config {
     const uint8_t id;
+    const char *bus_name;
 };
 
 static int greybus_control_init(struct device *dev) {
 
-	const struct greybus_control_config *config =
-			(const struct greybus_control_config *)dev->config_info;
+    struct greybus_control_config *config =
+        (struct greybus_control_config *)dev->config_info;
+    int r;
+    struct device *bus;
+    struct bus_api *api;
+
+    bus = device_get_binding(config->bus_name);
+    if (NULL == bus) {
+		LOG_ERR("control: failed to get binding for device '%s'", config->bus_name);
+    	return -ENODEV;
+    }
+
+    api = (struct bus_api *) bus->driver_api;
+    if (NULL == api) {
+		LOG_ERR("control: failed to get driver_api for '%s'", config->bus_name);
+    	return -EINVAL;
+    }
+
+    r = api->add_cport(bus, config->id, BUNDLE_CLASS_CONTROL, CPORT_PROTOCOL_CONTROL);
+    if (r < 0) {
+		LOG_ERR("control: failed to get driver_api for '%s'", config->bus_name);
+		return r;
+    }
 
     LOG_INF("probed cport %u: class: %u protocol: %u", config->id,
 		BUNDLE_CLASS_CONTROL, CPORT_PROTOCOL_CONTROL);
 
     return 0;
+}
+
+extern int defer_init(struct device *, int (*init)(struct device *));
+static int defer_greybus_control_init(struct device *dev) {
+	return defer_init(dev, &greybus_control_init);
 }
 
 #define DEFINE_GREYBUS_CONTROL(_num)										\
@@ -35,10 +64,12 @@ static int greybus_control_init(struct device *dev) {
 		static const struct greybus_control_config							\
 			greybus_control_config_##_num = {								\
                 .id = (uint8_t) _num,										\
+				.bus_name = 												\
+					DT_LABEL(DT_PARENT(DT_PARENT(DT_DRV_INST(_num)))),		\
         };																	\
         																	\
         DEVICE_INIT(gpio_control_##_num, "GBCONTROL_" #_num,				\
-                            greybus_control_init,							\
+                            defer_greybus_control_init,						\
 							NULL,											\
                             &greybus_control_config_##_num, POST_KERNEL,	\
                             CONFIG_KERNEL_INIT_PRIORITY_DEVICE);

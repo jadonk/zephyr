8b114392915816618d04af9d814484b49a499360
Christopher Friedt <chrisfriedt@gmail.com>
2020-11-02 15:15:16 -0500

    net: dns: enable dns service discovery for mdns responder
    This change enables support for DNS service discovery
    (RFC 6763) in the mdns_responder service and sample app.
    
    Fixes #29429
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

--- samples/net/mdns_responder/src/main.c
+++ samples/net/mdns_responder/src/main.c
@@ -10,7 +10,11 @@
 LOG_MODULE_REGISTER(net_mdns_responder_sample, LOG_LEVEL_DBG);
 
 #include <zephyr.h>
+#include <net/dns_sd.h>
 #include <net/net_core.h>
+
+DNS_SD_REGISTER_TCP_SERVICE(zephyr_tcp, CONFIG_NET_HOSTNAME, "_zephyr",
+	"local", DNS_SD_EMPTY_TXT, 4242);
 
 /* Note that this application does not do anything itself.
  * It is just a placeholder for waiting mDNS queries.

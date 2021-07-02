3f0f84b9db3529f615233d9483d6bbe37d17e7ec
Stancu Florin <niflostancu@gmail.com>
2021-07-02 16:50:59 +0300

    drivers: ieee802154: cc13xx_cc26xx: add subghz power saving
    When stopping the interface, also abort the RX routine and enable RF
    power saving. Will re-start RX on iface start.
    
    Also fixed a bug with `cmd_fs` crashing at RF wakeup because `rf_mode`
    was allocated on stack.
    
    Signed-off-by: Stancu Florin <niflostancu@gmail.com>

--- drivers/ieee802154/ieee802154_cc13xx_cc26xx_subg.c
+++ drivers/ieee802154/ieee802154_cc13xx_cc26xx_subg.c
@@ -74,6 +74,36 @@ static RF_Mode rf_mode = {
 	.cpePatchFxn = &rf_patch_cpe_multi_protocol,
 };
 
+#else
+static uint32_t overrides_sub_ghz[] = {
+       /* DC/DC regulator: In Tx, use DCDCCTL5[3:0]=0x7 (DITHER_EN=0 and IPEAK=7). */
+       (uint32_t)0x00F788D3,
+       /* Set RF_FSCA.ANADIV.DIV_SEL_BIAS = 1. Bits [0:16, 24, 30] are don't care.. */
+       (uint32_t)0x4001405D,
+       /* Set RF_FSCA.ANADIV.DIV_SEL_BIAS = 1. Bits [0:16, 24, 30] are don't care.. */
+       (uint32_t)0x08141131,
+       /* Tx: Configure PA ramp time, PACTL2.RC=0x3 (in ADI0, set PACTL2[4:3]=0x3) */
+       ADI_2HALFREG_OVERRIDE(0, 16, 0x8, 0x8, 17, 0x1, 0x1),
+       /* Tx: Configure PA ramping, set wait time before turning off (0x1A ticks of 16/24 us = 17.3 us). */
+       HW_REG_OVERRIDE(0x6028, 0x001A),
+       /* Rx: Set AGC reference level to 0x16 (default: 0x2E) */
+       HW_REG_OVERRIDE(0x609C, 0x0016),
+       /* Rx: Set RSSI offset to adjust reported RSSI by -1 dB (default: -2), trimmed for external bias and differential configuration */
+       (uint32_t)0x000188A3,
+       /* Rx: Set anti-aliasing filter bandwidth to 0x8 (in ADI0, set IFAMPCTL3[7:4]=0x8) */
+       ADI_HALFREG_OVERRIDE(0, 61, 0xF, 0x8),
+       /* Tx: Set PA trim to max to maximize its output power (in ADI0, set PACTL0=0xF8) */
+       ADI_REG_OVERRIDE(0, 12, 0xF8),
+       (uint32_t)0xFFFFFFFF
+};
+#endif
+
+/** RF patches to use (note: RF core keeps a pointer to this, so no stack). */
+static RF_Mode rf_mode = {
+	.rfMode = RF_MODE_MULTIPLE,
+	.cpePatchFxn = &rf_patch_cpe_multi_protocol,
+};
+
 /* Sub GHz power table */
 static const RF_TxPowerTable_Entry txPowerTable_sub_ghz[] = {
 	{ -20, RF_TxPowerTable_DEFAULT_PA_ENTRY(0, 3, 0, 2) },

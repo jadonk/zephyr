b8fd63ac1faa3292d802c05db91b5d068e67e7e8
Jason Kridner <jkridner@beagleboard.org>
2021-12-02 18:35:40 -0500

    sensortest: WIP

--- samples/boards/beagle_bcf/sensortest/src/main.c
+++ samples/boards/beagle_bcf/sensortest/src/main.c
@@ -127,8 +127,9 @@ static void led_work_handler(struct k_work *work)
 {
 	ARG_UNUSED(work);
 
-	/*
 	int r;
+
+	/*
 	uint8_t prev_led;
 
 	LOG_DBG("%s(): active_led: %u", __func__, led_work.active_led);
